750c83d7412c59f1041847e2c4b799e267175339
Kofi Dankwa <Kofi.dankwa1994@hotmail.com>
2016-07-18 06:40:03 +0100

    fixed a small bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   validates :username, presence: true, length: {maximum: 10}
   validates :full_name, length: {maximum: 20}
